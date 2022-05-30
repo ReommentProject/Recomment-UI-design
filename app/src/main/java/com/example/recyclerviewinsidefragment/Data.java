b5fab5d70cf4362a21af5d4c4e751219da33ea33
(shinhyo) <(tlsgyals1216@gmail.com)>
2022-05-31 00:28:43 +0900

    First commit to organization , add recyclerview to fragment

--- app/src/main/java/com/example/recyclerviewinsidefragment/Data.java
+++ app/src/main/java/com/example/recyclerviewinsidefragment/Data.java
@@ -2,12 +2,14 @@ package com.example.recyclerviewinsidefragment;
 
 public class Data {
     public static String[] names=new String[]{
-            "Android","java","C","C++","php"
+            "Android","java","C","C++","php", "Android","java","C","C++","php"
 
     };
 
     public static int[] images=new int[]{
             R.drawable.engineer,R.drawable.happy,R.drawable.lisa,
+            R.drawable.miss,R.drawable.sad , R.drawable.engineer,R.drawable.happy,R.drawable.lisa,
             R.drawable.miss,R.drawable.sad
+
     };
 }
