338610b8bafec69d3c2dc8aaee6bdbdee0ee8054
(shinhyo) <(tlsgyals1216@gmail.com)>
2022-05-30 23:10:44 +0900

    add Recyclerview in fragment

--- app/src/main/java/com/example/recyclerviewinsidefragment/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewinsidefragment/MainActivity.java
@@ -1,5 +1,57 @@
 package com.example.recyclerviewinsidefragment;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Recomment");
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new recyclerviewFragment()).commit();
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.item1:
+                        //fragment= new Fragment1();
+                        fragment=new recyclerviewFragment();
+                        FragmentManager fragmentManager=getSupportFragmentManager();
+                        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                        fragmentTransaction.add(R.id.frameLayout,fragment);
+                        fragmentTransaction.commit();
+                        break;
+                    case R.id.item2:
+                        fragment= new Fragment2();
+                        break;
+                    case R.id.item3:
+                        fragment= new Fragment3();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,fragment).commit();
+                return true;
+            }
+        });
+    }
+}
+/*
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -19,4 +71,4 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.add(R.id.frameLayout,recyclerviewFragment);
         fragmentTransaction.commit();
     }
-}
+}*/
