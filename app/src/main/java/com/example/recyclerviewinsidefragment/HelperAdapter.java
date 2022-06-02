a5d38841b1a518528770d16a89e19f61881b59a3
JeonghyeonK <6843wjdgus@naver.com>
2022-06-02 16:59:55 +0900

    합체

--- app/src/main/java/com/example/recyclerviewinsidefragment/HelperAdapter.java
+++ app/src/main/java/com/example/recyclerviewinsidefragment/HelperAdapter.java
@@ -1,6 +1,7 @@
 package com.example.recyclerviewinsidefragment;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import org.w3c.dom.Text;
 
@@ -38,6 +41,14 @@ public class HelperAdapter extends RecyclerView.Adapter {
         ViewHolderClass viewHolderClass=(ViewHolderClass)holder;
         viewHolderClass.textView1.setText(arrayList.get(position));
         viewHolderClass.textView2.setText(arrayListName.get(position));
+
+        String url="https://youtu.be/JNL44p5kzTk";
+        String id = url.substring(url.lastIndexOf("/")+1);  //맨마지막 '/'뒤에 id가있으므로 그것만 파싱해줌
+        Log.d("파싱한 아이디id 값", id);
+        String imgUrl ="https://img.youtube.com/vi/"+ id+ "/" + "default.jpg";  //유튜브 썸네일 불러오는 방법
+        Glide.with(context).load(imgUrl).into(viewHolderClass.imageView);
+        viewHolderClass.imageView.setVisibility(View.VISIBLE); //동영상이면 재생버튼도 보이게한다.
+
         viewHolderClass.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,10 +64,12 @@ public class HelperAdapter extends RecyclerView.Adapter {
     public class ViewHolderClass extends RecyclerView.ViewHolder
     {
         TextView textView1, textView2;
+        ImageView imageView;
         public ViewHolderClass(@NonNull View itemView) {
             super(itemView);
             textView1=(TextView)itemView.findViewById(R.id.textView1);
             textView2=(TextView)itemView.findViewById(R.id.textView2);
+            imageView=(ImageView) itemView.findViewById(R.id.imageview);
         }
     }
 }
